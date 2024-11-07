41307a9169bbf22e099ff9ce2816aec2b2ab2ae1
djarae <djaraestivill@gmail.com>
2024-11-07 02:42:39 -0300

    OK

--- smashnotes_back/src/main/java/com/example/smashnotes_back/SmashnotesBackController.java
+++ smashnotes_back/src/main/java/com/example/smashnotes_back/SmashnotesBackController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+// await fetch('http://127.0.0.1:8080/api/v1/democors');
 @RestController
 @RequestMapping("api/v1/democors")
 public class SmashnotesBackController {
