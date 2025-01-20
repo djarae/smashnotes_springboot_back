e3228e6d2dbbf5d8d298aa2d9e40235448e40882
djarae <51816647+djarae@users.noreply.github.com>
2025-01-20 16:12:04 -0300

    Respaldo de SQL posicion y registro TBLS actualizadas

--- src/main/java/smashnotest_back/configs/BDConfig.java
+++ src/main/java/smashnotest_back/configs/BDConfig.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public  class BDConfig {
     //Datos:
     //Datos a conexion a produccion
-      public static String url = "jdbc:postgresql://ep-shrill-cell-a5wddks2.us-east-2.aws.neon.tech:5432/neondb"; public static String user="neondb_owner"; public static String password="x2umTeFRS8bA";
+    //  public static String url = "jdbc:postgresql://ep-shrill-cell-a5wddks2.us-east-2.aws.neon.tech:5432/neondb"; public static String user="neondb_owner"; public static String password="x2umTeFRS8bA";
     //Datos a conexion a desarrollo
-   // public static String  url="jdbc:mysql://localhost:3306/smashbd"; public static String user="root"; public static String password="";
+   public static String  url="jdbc:mysql://localhost:3306/smashbd"; public static String user="root"; public static String password="";
 
 
     public static Connection Conexion= null;
