1b1d5b691c1383cb21428e9371a9a8282a92527f
djarae <51816647+djarae@users.noreply.github.com>
2025-02-04 14:59:43 -0300

    Deploy a produccion

--- src/main/java/smashnotest_back/configs/BDConfig.java
+++ src/main/java/smashnotest_back/configs/BDConfig.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public  class BDConfig {
     //Datos:
-    //Datos a conexion a produccion
+    //Datos a conexion a produccion a
      public static String url = "jdbc:postgresql://ep-shrill-cell-a5wddks2.us-east-2.aws.neon.tech:5432/neondb"; public static String user="neondb_owner"; public static String password="x2umTeFRS8bA";
     //Datos a conexion a desarrollo x
 //    public static String  url="jdbc:mysql://localhost:3306/smashbd"; public static String user="root"; public static String password="";
