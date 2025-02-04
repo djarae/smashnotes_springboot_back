7f4b8a2548f7b2ef477d5cdb2e8d687798d42032
djarae <51816647+djarae@users.noreply.github.com>
2025-02-04 14:53:15 -0300

    a

--- src/main/java/smashnotest_back/configs/BDConfig.java
+++ src/main/java/smashnotest_back/configs/BDConfig.java
@@ -8,7 +8,7 @@ public  class BDConfig {
     //Datos:
     //Datos a conexion a produccion
      public static String url = "jdbc:postgresql://ep-shrill-cell-a5wddks2.us-east-2.aws.neon.tech:5432/neondb"; public static String user="neondb_owner"; public static String password="x2umTeFRS8bA";
-    //Datos a conexion a desarrollo
+    //Datos a conexion a desarrollo x
 //    public static String  url="jdbc:mysql://localhost:3306/smashbd"; public static String user="root"; public static String password="";
 
 
