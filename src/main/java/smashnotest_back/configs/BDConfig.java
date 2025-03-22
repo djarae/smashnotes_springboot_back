0f84d0e6b340ff454cc3c918d0afe0dd7556dc95
djarae <51816647+djarae@users.noreply.github.com>
2025-03-22 01:00:49 -0300

    point develop

--- src/main/java/smashnotest_back/configs/BDConfig.java
+++ src/main/java/smashnotest_back/configs/BDConfig.java
@@ -9,7 +9,7 @@ public  class BDConfig {
     //Datos a conexion a produccion a
    //  public static String url = "jdbc:postgresql://ep-shrill-cell-a5wddks2.us-east-2.aws.neon.tech:5432/neondb"; public static String user="neondb_owner"; public static String password="x2umTeFRS8bA";
     //Datos a conexion a desarrollo x
-   public static String  url="jdbc:mysql://localhost:3306/smashbd"; public static String user="root"; public static String password="";
+   public static String  url="jdbc:postgresql://localhost:5432/postgres"; public static String user="postgres"; public static String password="mipostgre345";
 
 
     public static Connection Conexion= null;
