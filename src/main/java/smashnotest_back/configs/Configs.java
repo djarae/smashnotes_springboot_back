06a44d88e8fa0131b6e7cf32824fdfdceb6a7171
djarae <51816647+djarae@users.noreply.github.com>
2025-03-31 01:41:57 -0300

    apuntando a db de render db

--- src/main/java/smashnotest_back/configs/Configs.java
+++ src/main/java/smashnotest_back/configs/Configs.java
@@ -6,9 +6,13 @@ import java.sql.SQLException;
 
 public  class Configs {
     //Datos a conexion a produccion a
-     public static String url = "jdbc:postgresql://ep-shrill-cell-a5wddks2.us-east-2.aws.neon.tech:5432/neondb"; public static String user="neondb_owner"; public static String password="x2umTeFRS8bA";
+     //public static String url = "jdbc:postgresql://ep-shrill-cell-a5wddks2.us-east-2.aws.neon.tech:5432/neondb"; public static String user="neondb_owner"; public static String password="x2umTeFRS8bA";
     //Datos a conexion a desarrollo x
    // public static String  url="jdbc:postgresql://localhost:5432/postgres"; public static String user="postgres"; public static String password="mipostgre345";
+
+    //Coenxion render bd :
+public static String url = "jdbc:postgresql://dpg-cvdnmd3v2p9s7393g3e0-a.ohio-postgres.render.com:5432/smashbd"; public static String user="smash_user"; public static String password="IeuiOxRa3Pod3Ivi7CCAEwHTNm6s6PDg";
+
 
     public static Connection Conexion= null;
 
@@ -24,6 +28,8 @@ public  class Configs {
             System.out.println("Conexion exitosa");
         } catch (SQLException e) {
             System.out.println("Error en la conexion" + e);
+
+
         }
 
     }
