a8f50a03ae44a2725c85a682c2e8fd2df858b942
djarae <djaraestivill@gmail.com>
2025-11-29 21:10:13 -0300

    ataque y combo entidades

---
+++ src/main/java/smashnotest_back/matchups/data/entitys/Ataque.java
@@ -0,0 +1,48 @@
+package smashnotest_back.matchups.data.entitys;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ataque")
+public class Ataque {
+
+    @Id
+    private Integer id;
+    private String nombre;
+    private String abreviatura;
+
+    public Ataque() {
+    }
+
+    public Ataque(Integer id, String nombre, String abreviatura) {
+        this.id = id;
+        this.nombre = nombre;
+        this.abreviatura = abreviatura;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAbreviatura() {
+        return abreviatura;
+    }
+
+    public void setAbreviatura(String abreviatura) {
+        this.abreviatura = abreviatura;
+    }
+}
